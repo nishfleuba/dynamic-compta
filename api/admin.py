4fd3bb273ad24d077488fdbbd5719ebe3a65fb04
fleuba <nishfleuba@gmail.com>
2025-12-02 16:45:08 +0200

    model+admin

--- api/admin.py
+++ api/admin.py
@@ -1,4 +1,5 @@
-from django.contrib import admin
+from django.contrib import admin,messages
+from django.utils import timezone
 from .models import *
 
 @admin.register(PlanComptable)
@@ -6,11 +7,75 @@ class PlanComptableAdmin(admin.ModelAdmin):
     list_display = ('nom','numero', 'is_active', 'quantite_unitaire')
     list_filter = ('is_active',)
 
+from django.contrib import admin, messages
+from .models import Menu, HistoriqueMenu
+
 @admin.register(Menu)
 class MenuAdmin(admin.ModelAdmin):
-    list_display = ('nom', 'categorie', 'debiteur', 'crediteur', 'amortissement' , 'is_deleted', 'created_by')
+    list_display = ('nom', 'categorie', 'debiteur', 'crediteur', 'amortissement', 'is_deleted', 'created_by', 'validate_by')
     list_filter = ('is_deleted', 'created_by')
     search_fields = ('nom', 'categorie')
+    actions = ["valider_menu"]
+    filter_horizontal = ('permitted_users',)
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.created_by = request.user
+            obj.validate_by = None      
+            obj.validate_at = None      
+        super().save_model(request, obj, form, change)
+
+            
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(permitted_users=request.user)
+
+    def has_delete_permission(self, request, obj=None):
+        if obj:
+            if obj.validate_by or Journal.objects.filter(menu=obj).exists():
+                return False 
+        return super().has_delete_permission(request, obj)
+
+    def delete_model(self, request, obj):
+
+        if obj.validate_by or Journal.objects.filter(menu=obj).exists():
+            messages.error(request, f"Le menu '{obj.nom}' ne peut pas être supprimé car il a été validé ou journalisé.")
+            return
+        super().delete_model(request, obj)
+
+    def valider_menu(self, request, queryset):
+
+        for menu in queryset:
+
+            if menu.validate_by:
+                messages.error(request, f"Le menu '{menu.nom}' est déjà validé.")
+                return
+
+            menu.validate_by = request.user
+            menu.validate_at = timezone.now()
+            menu.save()
+
+            Journal.objects.create(
+                menu=menu,
+                created_by=request.user,
+                debiteur=menu.debiteur,
+                crediteur=menu.crediteur,
+                amortissement=menu.amortissement
+            )
+
+            HistoriqueMenu.objects.create(
+                menu=menu,
+                created_by=request.user,
+                motif=f"Validation du menu {menu.nom}"
+            )
+
+        messages.success(request, "Validation effectuée avec succès.")
+
+    valider_menu.short_description = "Valider menu"
+
 
 @admin.register(HistoriqueMenu)
 class HistoriqueMenuAdmin(admin.ModelAdmin):
--- api/models.py
+++ api/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class PlanComptable(models.Model):
     nom = models.CharField(max_length=100)
     numero = models.IntegerField()
-    is_active = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False,editable=False)
     quantite_unitaire = models.IntegerField()
 
     def __str__(self):
@@ -19,30 +19,34 @@ class Menu(models.Model):
     debiteur = models.ForeignKey(PlanComptable,on_delete=models.PROTECT, related_name='debiteur')
     crediteur = models.ForeignKey(PlanComptable,on_delete=models.PROTECT, related_name='crediteur')
     amortissement = models.ForeignKey(PlanComptable,on_delete=models.PROTECT, related_name='amortissement')
-    is_deleted = models.BooleanField(default=False)
-    created_by = models.ForeignKey(User, on_delete=models.PROTECT)
+    is_deleted = models.BooleanField(default=False,editable=False)
+    validate_by = models.ForeignKey(User,on_delete=models.PROTECT,null=True,blank=True,related_name='validate',editable=False)
+    validate_at = models.DateTimeField(null=True,blank=True,editable=False)
+    created_by = models.ForeignKey(User, on_delete=models.PROTECT,editable=False)
+    permitted_users = models.ManyToManyField(User, blank=True, related_name='menus_permis')
 
     def __str__(self):
         return f"{self.nom}-{self.debiteur} - {self.crediteur} - {self.amortissement}"
 
+class Journal(models.Model):
+
+    menu = models.ForeignKey(Menu, on_delete=models.PROTECT)
+    montant = models.DecimalField(max_digits=12, decimal_places=2,null=True)
+    debiteur = models.ForeignKey(PlanComptable, on_delete=models.PROTECT, related_name='journal_debiteur')
+    crediteur = models.ForeignKey(PlanComptable, on_delete=models.PROTECT, related_name='journal_crediteur')
+    amortissement = models.ForeignKey(PlanComptable, on_delete=models.PROTECT, related_name='journal_amortissement')
+    created_at = models.DateTimeField(auto_now_add=True,editable=False)
+    created_by = models.ForeignKey(User, on_delete=models.PROTECT,editable=False)
+
+    def __str__(self):
+        return f"{self.menu.nom} - {self.montant}-{self.debiteur} - {self.crediteur}"
+
 class HistoriqueMenu(models.Model):
     menu = models.ForeignKey(Menu, on_delete=models.PROTECT)
-    created_at = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey(User, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True,editable=False)
+    created_by = models.ForeignKey(User, on_delete=models.PROTECT,editable=False)
     motif = models.CharField(max_length=255)
 
     def __str__(self):
         return f"Historique de {self.menu} créé le {self.created_at} par {self.created_by}"
 
-class Journal(models.Model):
-
-    menu = models.ForeignKey(Menu, on_delete=models.PROTECT)
-    montant = models.DecimalField(max_digits=12, decimal_places=2)
-    debiteur = models.ForeignKey(PlanComptable, on_delete=models.PROTECT, related_name='journal_debiteur')
-    crediteur = models.ForeignKey(PlanComptable, on_delete=models.PROTECT, related_name='journal_crediteur')
-    amortissement = models.ForeignKey(PlanComptable, on_delete=models.PROTECT, related_name='journal_amortissement')
-    created_at = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey(User, on_delete=models.PROTECT)
-
-    def __str__(self):
-        return f"{self.menu.nom} - {self.montant}-{self.debiteur} - {self.crediteur}"
